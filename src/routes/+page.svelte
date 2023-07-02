e7b7c2405f40cc00680afd67771ed020ae29feec
ecila7290 <jihoon.kim89@gmail.com>
2023-07-02 15:34:56 +0900

    c: Add TodoItem component to page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
+<script>
+  import TodoItem from "$lib/todo-item.svelte";
+</script>
+
 <h1>Todos</h1>
 <form action="" method="">
   <input type="text" name="text" aria-label="Add a todo" placeholder="+ tap to add a todo">
 </form>
 
 
+<TodoItem />
